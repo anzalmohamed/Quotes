f10671330e65d22b54d6f49147fe4c73f907348c
anzalmohamed <anzaldidah@gmail.com>
2021-03-29 05:33:27 +0300

    making stylistic changes on the app

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,7 +8,7 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1, '','','',new Date()),
+    new Quote(1, 'The best way to get started is to quit talking and begin doing','Daniel','Anzal',new Date(2020,3,14)),
   ];
   toggleDetails(index:any){
     this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
